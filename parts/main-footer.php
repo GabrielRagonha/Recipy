bdfb2da89d0471aebf5e07100b97459b8791175e
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-03-08 03:41:56 -0300

    chore: modify header and footer

--- parts/main-footer.php
+++ parts/main-footer.php
@@ -1,1 +1,7 @@
-<p>Main footer</p>
+<footer>
+    
+</footer>
+
+<script id="mainScript" src="<?php wp_enqueue_script('script', get_template_directory_uri() . '/assets/js/script.min.js') ?>" defer></script>
+
+<?php wp_footer(); ?>
