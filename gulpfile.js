afe9874c54a8d8167c320c15fc9501dc31555555
Gabriel Rodrigues <gabrielRagonhaProfissional@outlook.com>
2023-03-08 03:40:03 -0300

    feat: add minify main js function

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
-// Default compile
-function defaultCompile() {
+// Default scss compile
+function defaultScssCompile() {
   return src('assets/default/style.scss')
   .pipe(
     sass({
@@ -29,6 +29,14 @@ function scssCompile() {
     .pipe(dest('assets/css'));
 }
 
+// Default js compile
+function defaultJsCompile() {
+  return src('script.js')
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js' }))
+  .pipe(dest('assets/js'))
+}
+
 //Js compile
 function jsCompile() {
   return src('src/scripts/**/*.js')
@@ -39,14 +47,16 @@ function jsCompile() {
 
 //Watch
 function watchCompilers() {
-  watch('assets/default/style.scss', defaultCompile);
+  watch('assets/default/style.scss', defaultScssCompile);
   watch('src/scss/**/*.scss', scssCompile);
+  watch('script.js', defaultJsCompile);
   watch('src/scripts/**/*.js', jsCompile);
 }
 
 exports.default = series(
-  defaultCompile,
+  defaultScssCompile,
   scssCompile,
+  defaultJsCompile,
   jsCompile,
   watchCompilers
 );
